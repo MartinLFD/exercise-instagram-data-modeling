4f41755fa69d5708a2cafa7215f46486ae212012
MartinLFD <168610198+MartinLFD@users.noreply.github.com>
2024-08-17 01:46:07 +0000

    Table_finish

--- src/models.py
+++ src/models.py
@@ -7,26 +7,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Follower(Base):
+    __tablename__ = 'Follower' #añadir llaves foráneas o primarias 
+    ID = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('u.from.id'))
+    user_to_id = Column(Integer, ForeignKey('u.to.id'))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+
+
+
+class User(Base):
+    __tablename__ = 'User'
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(60), nullable=False, unique=True )
+    first_name = Column(String(80), nullable=False )
+    last_name = Column(String(80), nullable=False )
+    email = Column(String(100), nullable=False, unique=True )
+    
+
+
+
+
+
+
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False )
+    author_id = Column(Integer, ForeignKey('author.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+
+
+
+
+class Post(Base):
+    __tablename__ = 'Post'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+
+
+
+class Media(Base):
+    __tablename__ = 'Media'
+    ID = Column(Integer, primary_key=True)
+    
+    url = Column(String(400), nullable= False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+
+
+    
+
+    
 
 ## Draw from SQLAlchemy base
 try:
